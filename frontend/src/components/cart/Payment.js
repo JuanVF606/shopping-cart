cb14f8c810dc20c7deb432fd0efd820f48e03dff
Juan Venegas <j.venegasfuenzalida@gmail.com>
2022-11-16 00:54:51 -0300

    "cambios no importantes"

--- frontend/src/components/cart/Payment.js
+++ frontend/src/components/cart/Payment.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect } from 'react'
 
 import MetaData from '../layout/MetaData'
-import CheckoutSteps from './CheckoutSteps'
 
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
