ae9323e88527de90729ddda93e330445e9191657
Juan Venegas Fuenzalida <j.venegasfuenzalida@gmail.com>
2022-11-28 13:28:03 -0300

    restore db

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const ConnectMongoDB = () => {
     try {
-        mongoose.connect(process.env.DB_URI,{
+        mongoose.connect(process.env.DB_LOCAL_URL,{
             useNewUrlParser:true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
+            useUnifiedTopology:true
         }).then(con =>{
             console.log(`MongoDB Database conneccted  with HOST: ${con.connection.host}`);
         })
